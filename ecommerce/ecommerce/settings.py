0d19c6d4d1f3b0dd5a011a0075114597b53dfd33
Liberbell <liberbell@gmail.com>
2025-01-20 08:32:45 +0900

    settings3

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -120,6 +120,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = [BASE_DIR / 'static/media']
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
