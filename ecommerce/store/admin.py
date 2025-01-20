4740c27bac3ee542f01fb7c3a6dab9ab5e0c88eb
Liberbell <liberbell@gmail.com>
2025-01-21 08:58:10 +0900

    admin3

--- ecommerce/store/admin.py
+++ ecommerce/store/admin.py
@@ -6,4 +6,9 @@ from .models import Category, Product
 # admin.site.register(Product)
 
 @admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ("name",)}
+
 @admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ("name",)}
