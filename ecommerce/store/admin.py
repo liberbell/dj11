a7d90345fafed5b03a203990c6b13ac5bd8928a6
Liberbell <liberbell@gmail.com>
2025-01-21 08:51:28 +0900

    admin2

--- ecommerce/store/admin.py
+++ ecommerce/store/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 from .models import Category, Product
 
 # Register your models here.
-admin.site.register(Category)
-admin.site.register(Product)
+# admin.site.register(Category)
+# admin.site.register(Product)
+
+@admin.register(Category)
+@admin.register(Product)
