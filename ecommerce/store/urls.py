f58737b5276b9c35272dc2322e9140f8665e385a
Liberbell <liberbell@gmail.com>
2025-01-24 08:48:43 +0900

    urls3

--- ecommerce/store/urls.py
+++ ecommerce/store/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.store, name="store"),
     path('dashboard/', views.store, name="dashboard"),
-    path('logout/', views.store, name="user-logout")
+    path('logout/', views.store, name="user-logout"),
+    path('cart_summary/', views.store, name="cart-summary"),
 ]
