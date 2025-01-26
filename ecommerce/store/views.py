2b5ba57b3e1c4dfd2415e087614ea6740b2c8e29
Liberbell <liberbell@gmail.com>
2025-01-27 08:21:37 +0900

    views1

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from . models import Category, Product
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 def store(request):
@@ -11,3 +12,7 @@ def categories(request):
     all_categories = Category.objects.all()
 
     return {'all_categories': all_categories}
+
+
+def product_info(request, slug):
+    product = get_object_or_404(Product, slug=slug)
