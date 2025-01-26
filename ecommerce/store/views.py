6db25ccc4e1e5de466e2b4731dde817a57ba7975
Liberbell <liberbell@gmail.com>
2025-01-26 15:52:03 +0900

    views5

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -5,7 +5,7 @@ from . models import Category, Product
 def store(request):
     all_products = Product.objects.all()
     context = {'my_products': all_products}
-    return render(request, 'store/store.html')
+    return render(request, 'store/store.html', context)
 
 def categories(request):
     all_categories = Category.objects.all()
