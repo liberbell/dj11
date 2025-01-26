05a4459fb6e9db9d21158264cb308d28c44fa255
Liberbell <liberbell@gmail.com>
2025-01-26 15:21:46 +0900

    views1

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from . models import Category
 
 # Create your views here.
 def store(request):
     return render(request, 'store/store.html')
+
+def categories(request):
+    all_categories = Category.objects.all()
