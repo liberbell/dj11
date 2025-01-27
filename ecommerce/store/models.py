d2bfc45d3829736929d0ada20df2d9f3f8388b67
Liberbell <liberbell@gmail.com>
2025-01-28 08:29:45 +0900

    models1

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Category(models.Model):
@@ -29,3 +30,8 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse("product-info", kwargs={self.slug})
+    
+
