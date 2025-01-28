3fefa4c6f6dc146caff45587ed2448fbe12d127d
Liberbell <liberbell@gmail.com>
2025-01-28 19:57:46 +0900

    urls1

--- ecommerce/store/urls.py
+++ ecommerce/store/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('register/', views.store, name="register"),
     path('my_login/', views.store, name="my-login"),
     path('product/<slug:slug>/', views.product_info, name="product-info"),
+    path('search/<slug:slug>/', views.list_category, name="list-category"),
 ]
