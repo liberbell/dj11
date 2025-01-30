d2eee952d74007b30fc92f312617a831c557ea53
Liberbell <liberbell@gmail.com>
2025-01-30 09:28:06 +0900

    url5

--- ecommerce/cart/urls.py
+++ ecommerce/cart/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.cart_summary, name="cart-summary"),
     path('add/', views.cart_add, name="cart-add"),
-    path('delete/<pk:int>', views.cart_delete, name="cart-delete"),
-    path('update/<pk:int>', views.cart_update, name="cart-update"),
+    path('delete/<slug:slug>', views.cart_delete, name="cart-delete"),
+    path('update/<slug:int>', views.cart_update, name="cart-update"),
 ]
