b2302407ba7a3d6094305fc7b9f0d8ec1a47fe87
Liberbell <liberbell@gmail.com>
2025-02-01 10:11:03 +0900

    cart2

--- ecommerce/cart/cart.py
+++ ecommerce/cart/cart.py
@@ -3,3 +3,9 @@ class cart():
     
     def __init__(self, request):
         self.session = request.session
+        cart = self.session.get("session_key")
+
+        if "session_key" not in request.session:
+            cart = self.session["session_key"] = {}
+
+        self.cart = cart
