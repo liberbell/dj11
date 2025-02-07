0a6ffcc60cd4224951657bc2d591dbf23effa197
Liberbell <liberbell@gmail.com>
2025-02-07 09:07:06 +0900

    views1

--- ecommerce/cart/views.py
+++ ecommerce/cart/views.py
@@ -29,7 +29,16 @@ def cart_add(request):
 
 def cart_delete(request):
 
-    pass
+    cart=Cart(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get("product_id"))
+        cart.delete()
+
+        cart_quantity = cart.__len__()
+        response = JsonResponse({'qty ': cart_quantity})
+
+        return response
+    
 
 def cart_update(request):
 
