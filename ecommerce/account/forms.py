c62bb8c7986cdbe1242e4ec3b4547ccb1e172581
Liberbell <liberbell@gmail.com>
2025-02-10 08:53:34 +0900

    forms3

--- ecommerce/account/forms.py
+++ ecommerce/account/forms.py
@@ -1,8 +1,18 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django import forms
 
 class CreateUserForm(UserCreationForm):
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password', 'password2']
+
+    def __init__(self, *args, **kwargs):
+        super(CreateUserForm, self).__init__(*args, **kwargs)
+
+    def cleanemail(self):
+        email = self.cleaned_data("email")
+
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError()
