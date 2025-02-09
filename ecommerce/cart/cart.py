3d777873fd55dc4b93d7a5bd299d8337e18c5356
Liberbell <liberbell@gmail.com>
2025-02-09 15:17:45 +0900

    cart3

--- ecommerce/cart/cart.py
+++ ecommerce/cart/cart.py
@@ -1,5 +1,6 @@
 from decimal import Decimal
 from store.models import Product
+import copy
 
 class Cart():
     
@@ -46,7 +47,8 @@ class Cart():
     def __iter__(self):
         all_product_ids = self.cart.keys()
         products = Product.objects.filter(id__in=all_product_ids)
-        cart = self.cart.copy()
+        # cart = self.cart.copy()
+        cart = copy.deepcopy()
 
         for product in products:
             cart[str(product.id)]["product"] = product
