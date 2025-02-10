4e1cdb53aa73bfcd2e2691caaa728cfda5df3930
Liberbell <liberbell@gmail.com>
2025-02-10 09:07:37 +0900

    views2

--- ecommerce/account/views.py
+++ ecommerce/account/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def register(request):
-    return HttpResponse("Account url setup.")
+    return render("registration/register.html")
