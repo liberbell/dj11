451a09e94bccb0bcc786b708c975841cecc3d3fb
Liberbell <liberbell@gmail.com>
2025-02-11 11:42:42 +0900

    views1

--- ecommerce/account/views.py
+++ ecommerce/account/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .forms import CreateUserForm
+
 
 # Create your views here.
 def register(request):
+    form = CreateUserForm()
+
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
     return render(request, "account/registration/register.html")
