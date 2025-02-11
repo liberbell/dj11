07fe7d11dab16768cccb3963e201c9a793757761
Liberbell <liberbell@gmail.com>
2025-02-12 08:13:29 +0900

    settings2

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -46,7 +46,8 @@ INSTALLED_APPS = [
     'crispy_bootstrap5',
 ]
 
-CRISPY_TEMPLATE_PACK = 'Bootstrap 5'
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
