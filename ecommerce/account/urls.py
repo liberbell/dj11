decbe5e005f0cbaf1f4cd114f752281247260fb6
Liberbell <liberbell@gmail.com>
2025-02-13 08:44:23 +0900

    urls1

--- ecommerce/account/urls.py
+++ ecommerce/account/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register, name="register"),
+    path('email-verification/', views.email_verification, name="email-verification"),
 ]
