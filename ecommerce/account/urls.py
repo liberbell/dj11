f9c3b6cd9166ca21d15ddae67304f501a89deec5
Liberbell <liberbell@gmail.com>
2025-02-13 08:49:25 +0900

    urls3

--- ecommerce/account/urls.py
+++ ecommerce/account/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('email-verification/', views.email_verification, name="email-verification"),
     path('email-verification-sent/', views.email_verification_sent, name="email-verification-sent"),
     path('email-verification-success/', views.email_verification_success, name="email-verification-success"),
-    path('email-verification-failed/', views.email_verification_failed(), name="email-verification-failed"),
+    path('email-verification-failed/', views.email_verification_failed, name="email-verification-failed"),
 ]
