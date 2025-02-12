1e3b3ebe153917258000789d86f649a3997abd7d
Liberbell <liberbell@gmail.com>
2025-02-13 08:20:17 +0900

    forms2

--- ecommerce/account/forms.py
+++ ecommerce/account/forms.py
@@ -6,7 +6,7 @@ class CreateUserForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password', 'password2']
+        fields = ['username', 'email', 'password1', 'password2']
 
     def __init__(self, *args, **kwargs):
         super(CreateUserForm, self).__init__(*args, **kwargs)
