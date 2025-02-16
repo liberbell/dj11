88073379c90e6469679aaf8b54170c11726e2834
Liberbell <liberbell@gmail.com>
2025-02-16 09:45:40 +0900

    settings2

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -141,4 +141,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
-EMAIL_USE_TLS = True
+EMAIL_USE_TLS = 'True'
+
+EMAIL_HOST_USER = 'someone@gmail.com'
+EMAIL_HOST_PASSWORD = 'somepassword'
