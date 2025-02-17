f74909bdc12ecd8d12cc956e951de192a14c0c90
Liberbell <liberbell@gmail.com>
2025-02-18 08:12:42 +0900

    settings1

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -143,5 +143,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = 'True'
 
-EMAIL_HOST_USER = 'someone@gmail.com'
+EMAIL_HOST_USER = 'libersample67@gmail.com'
 EMAIL_HOST_PASSWORD = 'somepassword'
