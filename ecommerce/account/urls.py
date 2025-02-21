7755f056907ef0e4b86b30f890f1fb9bb83dc2b0
Liberbell <liberbell@gmail.com>
2025-02-21 09:00:54 +0900

    urls1

--- ecommerce/account/urls.py
+++ ecommerce/account/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('my-login/', views.my_login, name="my-login"),
     path('dashboard/', views.dashboard, name="dashboard"),
     path("user-logout/", views.user_logout, name="user-logout"),
+    path('profile-management/', views.profile_management, name="profile-management"),
+    path('delete-account/<int:pk>', views.delete_account, name="delete-account"),
 ]
