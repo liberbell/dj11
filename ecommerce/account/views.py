1696d4c0ac529e5785ed62f83c9ba42376a0a5a8
Liberbell <liberbell@gmail.com>
2025-02-25 08:32:58 +0900

    views1

--- ecommerce/account/views.py
+++ ecommerce/account/views.py
@@ -101,13 +101,14 @@ def dashboard(request):
 @login_required(login_url="my-login")
 def profile_management(request):
 
+    user_form = UpdateUserForm(instance=request.user)
+
     if request.method == "POST":
         user_form = UpdateUserForm(request.POST, instance=request.user)
         if user_form.is_valid():
             user_form.save()
             return redirect("dashboard")
         
-    user_form = UpdateUserForm(instance=request.user)
     context = {"user_form": user_form}
         
     return render(request, "account/profile_management.html", context=context)
