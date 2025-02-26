be4c49738d30d736f46fb674f4eea49c639c1910
Liberbell <liberbell@gmail.com>
2025-02-26 16:49:17 +0900

    urls1

--- ecommerce/account/urls.py
+++ ecommerce/account/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register/', views.register, name="register"),
@@ -12,4 +13,7 @@ urlpatterns = [
     path("user-logout/", views.user_logout, name="user-logout"),
     path('profile-management/', views.profile_management, name="profile-management"),
     path('delete-account/', views.delete_account, name="delete-account"),
+
+    path('reset_password/', auth_views.PasswordResetView.as_view(), name="reset_password"),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name="reset_password_done"),
 ]
