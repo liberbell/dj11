0d9e598a5d1b28a242289a8d1036f1cf8f5f9eee
Liberbell <liberbell@gmail.com>
2025-02-26 16:53:13 +0900

    urls2

--- ecommerce/account/urls.py
+++ ecommerce/account/urls.py
@@ -15,5 +15,6 @@ urlpatterns = [
     path('delete-account/', views.delete_account, name="delete-account"),
 
     path('reset_password/', auth_views.PasswordResetView.as_view(), name="reset_password"),
-    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name="reset_password_done"),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
 ]
