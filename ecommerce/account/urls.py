ee94a5ea55d15812854eed3232be46c329e83dca
Liberbell <liberbell@gmail.com>
2025-02-28 08:37:25 +0900

    urls2

--- ecommerce/account/urls.py
+++ ecommerce/account/urls.py
@@ -17,5 +17,5 @@ urlpatterns = [
     path('reset_password/', auth_views.PasswordResetView.as_view(template_name="account/password/password_reset.html"), name="reset_password"),
     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="account/password/password_reset_sent.html"), name="password_reset_done"),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="account/password/password_reset_form.html"), name="password_reset_confirm"),
-    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="account/password/password_reset_complete.html"), name="password_reset_complete"),
 ]
