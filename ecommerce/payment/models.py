b8ea0eb7558b442b264bfb0d3791a4fe2c093579
Liberbell <liberbell@gmail.com>
2025-03-05 08:39:27 +0900

    models1

--- ecommerce/payment/models.py
+++ ecommerce/payment/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class ShippingAddress(models.Model):
+    full_name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+    address1 = models.CharField(max_length=1024)
+    address2 = models.CharField(max_length=1024)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=255, null=True, blank=True)
