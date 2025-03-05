14951861d73c9d9b08bb0f7843bcbabe2453d570
Liberbell <liberbell@gmail.com>
2025-03-06 08:39:45 +0900

    forms2

--- ecommerce/payment/forms.py
+++ ecommerce/payment/forms.py
@@ -6,3 +6,4 @@ class ShippingForm(forms.ModelForm):
     class Meta:
         
         model = ShippingAddress
+        fields = ["full_name", "email", "address1", "address2", "city", "state", "zipcode"]
