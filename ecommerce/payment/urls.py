27bad7f5a618af21f973b9a31e3ec9efe4178832
Liberbell <liberbell@gmail.com>
2025-03-09 10:35:40 +0900

    urls1

--- ecommerce/payment/urls.py
+++ ecommerce/payment/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('payment-success', views.payment_success, name="payment-success"),
     path('payment-failed', views.payment_failed, name="payment-failed"), 
+    path('checkout/', views.checkout, name="checkout"),
 ]
