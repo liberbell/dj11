9a3787c19e64b55fe12862c3b0bdf1e370073e9d
Liberbell <liberbell@gmail.com>
2025-03-10 08:31:27 +0900

    models4

--- ecommerce/payment/models.py
+++ ecommerce/payment/models.py
@@ -30,3 +30,8 @@ class Order(models.Model):
     shipping_address = models.TextField(max_length=1000)
     amount_paid = models.DecimalField(max_digits=8, decimal_places=2)
     date_orderd = models.DateTimeField(auto_now_add=True)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return "order - #" + str(self.id)
