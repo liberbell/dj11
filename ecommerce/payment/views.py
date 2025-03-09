3271d89f7583d356bd08d6e791a5a3caf75ba012
Liberbell <liberbell@gmail.com>
2025-03-09 10:34:39 +0900

    views1

--- ecommerce/payment/views.py
+++ ecommerce/payment/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def checkout(request):
+    return render(request, "payment/checkout.html")
+
 def payment_success(request):
 
     return render(request, "payment/payment_success.html")
