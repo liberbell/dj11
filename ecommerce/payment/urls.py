808c7d7155c7e302cf912b95b85eddcc51b8de5e
Liberbell <liberbell@gmail.com>
2025-03-11 08:25:51 +0900

    urls1

--- ecommerce/payment/urls.py
+++ ecommerce/payment/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('payment-success', views.payment_success, name="payment-success"),
     path('payment-failed', views.payment_failed, name="payment-failed"), 
     path('checkout/', views.checkout, name="checkout"),
+    path('complete-order/', views.complete_order, name="complete-order"),
 ]
